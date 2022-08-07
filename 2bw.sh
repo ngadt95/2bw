78d08e944696d1d9e1f5b9580d1085f62c01f477
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-08-08 00:07:53 +0700

    Update 2bw.sh

--- 2bw.sh
+++ 2bw.sh
@@ -1,3 +1,4 @@
+setup.sh
 #!/bin/sh
 #Install docker and pull docker image
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -13,7 +14,7 @@ sed -i "s/(which iptables)/(which iptables-legacy)/g" /etc/miniupnpd/miniupnpd_f
 sed -i "s/(which ip6tables)/(which ip6tables-legacy)/g" /etc/miniupnpd/miniupnpd_functions.sh
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 
-
+run.sh
 #!/bin/sh
 [ "$#" = "0" ] && 
 echo 'Please enter public key.\nUsage: ./run.sh 831732497809ED0C1B58A59CC0EEA28D56B955093D06E3F012CDFDE8B2597777' && exit 1
