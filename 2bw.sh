1e8fae4b0c90dd330ce9eb97a2d10b74ffa9a19d
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-08-07 23:54:33 +0700

    2bw.sh

---
+++ 2bw.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#Install docker and pull docker image
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+docker pull sportstvdev/uam
+#Install miniupnpd
+apt install miniupnpd -y
+#Change miniupnpd and systemctl config
+sed -i "s/After=network-online.target/After=network-online.target docker.service/g" /etc/systemd/system/multi-user.target.wants/miniupnpd.service
+systemctl daemon-reload
+sed -i "s/#secure_mode=yes/secure_mode=yes/g" /etc/miniupnpd/miniupnpd.conf
+sed -i "s/(which iptables)/(which iptables-legacy)/g" /etc/miniupnpd/miniupnpd_functions.sh
+sed -i "s/(which ip6tables)/(which ip6tables-legacy)/g" /etc/miniupnpd/miniupnpd_functions.sh
+sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
