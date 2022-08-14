f5468de6ca64395a045d57de79c422b7338e60ec
ngadt95 <99725055+ngadt95@users.noreply.github.com>
2022-08-14 23:48:53 +0700

    Update 2bw.sh

--- 2bw.sh
+++ 2bw.sh
@@ -13,6 +13,7 @@ sudo chmod u+x setup.sh run.sh
 eth0
 docker0
 reboot
+swapon /var/swap.img
 systemctl restart miniupnpd.service
 systemctl status miniupnpd.service
 ./run.sh 831732497809ED0C1B58A59CC0EEA28D56B955093D06E3F012CDFDE8B2597777
